5e9b9cd32531ab74eefad29fe9bdef6f3e40df26
mattcaswell <matthewcaswell253@Gmail.com>
2017-09-26 13:34:42 -0700

    add animated side-nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KellyJo Catering';
+
+  ngAfterViewInit(){
+    $(function() {
+
+      var navBtn = $('.nav-btn');
+      function toggleNav() {
+          navBtn.toggleClass('open');
+          $('nav').toggleClass('open');
+          $('.container').toggleClass('open');
+      }
+      navBtn.click(function() {
+          toggleNav();
+      })
+      $('nav ul li a').click(function(e) {
+          toggleNav();
+      })
+  })
+  }
 }
